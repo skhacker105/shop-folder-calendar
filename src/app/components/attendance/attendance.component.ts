40b7a1bdfcb09f70c69cd72c3cec665df1502763
Diksha Bharti <dikshab0202@gmail.com>
2024-02-07 23:38:16 +0530

    calendar basic added

--- src/app/components/attendance/attendance.component.ts
+++ src/app/components/attendance/attendance.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Attendance } from '../../models';
 import { AttendanceState } from 'shop-folder-core';
+import { CommonModule } from '@angular/common';
+import { DynamicLayoutComponent } from 'shop-folder-component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { takeUntil } from 'rxjs';
+import { AttendanceService } from '../../services/attendance.service';
 
 @Component({
   selector: 'app-attendance',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    CommonModule,
+    DynamicLayoutComponent,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCardModule
+  ],
   templateUrl: './attendance.component.html',
   styleUrl: './attendance.component.scss'
 })
 export class AttendanceComponent implements OnInit {
+  selected: Date | null | undefined;
 
-  constructor(){}
+  constructor(private attendanceService:AttendanceService){}
   ngOnInit(): void {
     let createdBy = 'Saurabh Kumar';
     let state = AttendanceState.login;
@@ -28,4 +46,5 @@ export class AttendanceComponent implements OnInit {
       isSelected: true
     })
   }
+
 }
